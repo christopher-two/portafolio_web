95b1ea07ceaf8623035f19d7391a853e1df84e2e
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-07-13 23:12:18 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="traba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
           </div>
         </section>
 
-        <section id="trabajos" className="w-full py-12 md:py-24 lg:py-32 bg-secondary/50">
+        <section id="trabajos" className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center mb-12">
               <div className="space-y-2">
