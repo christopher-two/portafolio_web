e8843f3d4b51da7df2a40ef2af03ec5ebf58e19d
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-07-13 23:10:19 +0000

    url('https://fonts.googleapis.com/css2?family=Rubik+Glitch&display=swap'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
-                <h1 className="text-4xl font-bold font-headline tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
+                <h1 className="text-4xl font-glitch tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
                   Christopher Two
                 </h1>
                 <p className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ export default {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['"Space Grotesk"', 'sans-serif'],
+        glitch: ['"Rubik Glitch"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
