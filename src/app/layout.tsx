0b46c0f1b5367b3e446af97b27a1bc1ee2db5bf9
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-07-13 22:47:57 +0000

    haz que la pagina ocupe correctamente el maximo de widith y tambien el t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <div className="container">
-          {children}
-        </div>
+        {children}
         <Toaster />
       </body>
     </html>
