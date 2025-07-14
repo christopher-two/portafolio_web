16434f7c2a6372acb319e8b6a1bbda4fc6e3f8bf
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-07-14 01:10:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-6x

--- src/app/sobre-mi/page.tsx
+++ src/app/sobre-mi/page.tsx
@@ -9,7 +9,7 @@ export default function SobreMiPage() {
         <section id="sobre-mi" className="w-full">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
-              <h1 className="text-6xl font-bold font-montserrat tracking-tighter sm:text-8xl md:text-9xl">Yo soy Christopher</h1>
+              <h1 className="text-3xl font-bold font-montserrat tracking-tighter sm:text-4xl md:text-5xl">Yo soy Christopher</h1>
               <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Un poco sobre mi, mi trayectoria y mis habilidades.
               </p>
