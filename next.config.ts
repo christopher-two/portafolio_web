74ee715df99610da64f001ac91fb217e9fa21e6d
christopher-two <christopher.cop787@gmail.com>
2025-08-02 05:02:44 -0600

    Bugfix images 1

--- next.config.ts
+++ next.config.ts
@@ -55,10 +55,14 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
